2c60dc1f84ee53ea733024cd76c5385989604a32
janani <janrid23456@gmail.com>
2025-02-17 16:05:49 +0530

    adding the user entity

---
+++ src/main/java/module/User.java
@@ -0,0 +1,33 @@
+package module;
+
+public class User {
+    int id;
+    String name;
+    String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+
+
+    }
+}
